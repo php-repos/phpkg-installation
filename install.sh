347ea0cfb7b40c41f8a6a8f8b3e067ab4629eca5
Morteza Poussaneh <49806042+MortezaPoussaneh@users.noreply.github.com>
2024-06-23 11:15:51 +0200

    release v1.7.2

--- install.sh
+++ install.sh
@@ -21,7 +21,7 @@ file_manager_path=$packages_path/file-manager
 control_flow_path=$packages_path/control-flow
 console_path=$packages_path/console
 
-phpkg_version="v1.7.1"
+phpkg_version="v1.7.2"
 cli_version="v2.0.0"
 datatype_version="v1.2.0"
 file_manager_version="v2.0.3"
